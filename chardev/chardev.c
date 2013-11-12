35908551cc88bb729d0ad02a7ec232a950712ab3
root <root@localhost.localdomain>
2013-11-11 23:59:21 -0500

    added chardev files to repo

---
+++ chardev/chardev.c
@@ -0,0 +1,85 @@
+#include "chardev.h"
+
+/* Globals localized to file (by use of static */
+static int Major;		/* assigned to device driver */
+static char msg[BUF_LEN];	/* a stored message */
+
+static struct file_operations fops = {
+	.read = device_read,
+	.write = device_write,
+	.open = device_open,
+	.release = device_release
+};
+
+static int device_open(struct inode *inode, struct file *file)
+{
+	try_module_get(THIS_MODULE);
+
+	return 0;
+}
+
+static int device_release(struct inode *inode, struct file *file)
+{
+	module_put(THIS_MODULE);
+	return 0;
+}
+
+/* Called when a process writes to dev file: echo "hi" > /dev/hello */
+static ssize_t device_write(struct file *filp, const char *buff,
+			    size_t len, loff_t * off)
+{
+	int copy_len = len > BUF_LEN ? BUF_LEN : len;
+	unsigned long amnt_copied = 0;
+
+	/* NOTE: copy_from_user returns the amount of bytes _not_ copied */
+	amnt_copied = copy_from_user(msg, buff, copy_len);
+	if (copy_len == amnt_copied)
+		return -EINVAL;
+
+	return copy_len - amnt_copied;
+}
+
+static ssize_t device_read(struct file *filp, char *buffer, size_t len,
+			   loff_t * offset)
+{
+	unsigned long amnt_copied;
+	int amnt_left = BUF_LEN - *offset;
+	char *copy_position = msg + *offset;
+	int copy_len = len > amnt_left ? amnt_left : len;
+
+	/* are we at the end of the buffer? */
+	if (amnt_left <= 0)
+		return 0;
+
+	/* NOTE: copy_to_user returns the amount of bytes _not_ copied */
+	amnt_copied = copy_to_user(buffer, copy_position, copy_len);
+	if (copy_len == amnt_copied)
+		return -EINVAL;
+
+	/* adjust the offset for this process */
+	*offset += copy_len;
+
+	return copy_len - amnt_copied;
+}
+
+int init_module(void)
+{
+	Major = register_chrdev(0, DEVICE_NAME, &fops);
+
+	if (Major < 0) {
+		printk(KERN_ALERT "Failed to register char device.\n");
+		return Major;
+	}
+
+	memset(msg, '+', BUF_LEN);
+	printk(KERN_INFO "chardev is assigned to major number %d.\n",
+	       Major);
+
+	return 0;
+}
+void cleanup_module(void)
+{
+	int ret = unregister_chrdev(Major, DEVICE_NAME);
+	if (ret < 0)
+		printk(KERN_ALERT "Error in unregister_chrdev: %d\n", ret);
+}
---
+++ chardev/test_r.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	int fd;
+	char stuff[16];
+	char other[16];
+	memset(stuff,0,16);
+	memset(other,0,16);
+	snprintf(stuff,sizeof(char)*16,"my name is");
+	printf("%s \n",other);
+	fd = open("/dev/filename_holder",O_RDWR);
+	write(fd, stuff, sizeof(stuff));
+	printf("%s \n",other);
+	read(fd, other, sizeof(other));
+	printf("%s \n",other);
+	return 0;
+
+}
