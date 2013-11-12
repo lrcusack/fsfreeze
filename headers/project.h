856b244f6e105dd308b3f6ffd96ffae6e42029f4
austin <ajblack@gwmail.gwu.edu>
2013-11-12 14:13:08 -0500

    added comments to project.h for my use

--- headers/project.h
+++ headers/project.h
@@ -11,9 +11,10 @@
 #define LOG_FORMAT "%d %s"
 #define DELIM '\n'
 
+/*make the userspace helper take one of these enums */
 typedef enum{
-	CREATE,
-	MODIFY,
-	DELETE
+	CREATE, //write to the log
+	MODIFY, //writes to the log, makes a copy of the file
+	DELETE  //makes a copy of the file that we are deleting
 } change_type_t;
 
