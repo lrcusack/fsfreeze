d2785ec35c41a4be9c599e1aaa23d155a881f580
lrcusack <lrcusack7@gmail.com>
2013-11-17 23:50:16 -0500

    Delete .mod_write.ko.cmd

--- sys_calls/write/.mod_write.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/fsfreeze/sys_calls/write/mod_write.ko := ld -m elf_i386 -m elf_i386 -r -o /root/fsfreeze/sys_calls/write/mod_write.ko /root/fsfreeze/sys_calls/write/mod_write.o /root/fsfreeze/sys_calls/write/mod_write.mod.o
