e2934e4d996fcf888f43f334d8d94135fe0a8678
Liam Cusack <lrcusack7@gmail.com>
2013-11-21 17:44:13 -0500

    added starting code for userspace helper

---
+++ userspace_helper/helper.c
@@ -0,0 +1,28 @@
+#include "../headers/project.h>"
+#include<stdlib.h>
+#include<stdio.h>
+
+void handle_change(char* fname, change_type_t type){
+	
+	switch(type){
+//############################################################
+		case CREATE :
+			//write fname to LOG_NAME (using LOG_FORMAT)
+			
+			
+		break;
+//############################################################		
+		case MODIFY :
+			//write fname to LOG_NAME (using LOG_FORMAT)
+			//save a copy of fname to RESTOREDIR
+			
+			
+		break;
+//############################################################		
+		case DELETE :
+			//save a copy of fname to RESTOREDIR
+			
+			
+		break;
+//############################################################
+	}
