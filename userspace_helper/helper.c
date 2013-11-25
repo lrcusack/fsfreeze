5d0d82885a5ec935e7517caac1cb004e0328a71e
austin <ajblack@gwmail.gwu.edu>
2013-11-25 01:51:55 -0500

    this is still not working.  in the copy clean function i have those two print statements.  Somehow, my copy file is getting messed up and a couple random characters are being added for seemingly no reason

--- userspace_helper/helper.c
+++ userspace_helper/helper.c
@@ -1,20 +1,18 @@
 //### austin blackman ###*/
 
 
-//#include "../headers/project.h>"
+#include "../headers/project.h"
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 FILE *f;
 FILE *f2;
 char ch;
 
-int main(){
-	handle_change('M');
 
-	return 0;
-}
 
-void write_to_log(char type){
+//
+void write_to_log(char type,FILE* file_name){
 	
 			
 	f = fopen("logfile.txt","w");
@@ -29,30 +27,48 @@ void write_to_log(char type){
 	fclose(f);		
 }
 
-void make_log_copy(char type){
-	f = fopen("logfile.txt","r");
-	f2 = fopen("logcopy.txt","w");
-	
-	while(1){
-		ch = fgetc(f);
-		if(ch==EOF){
-			break;
-		}
-		else{
-			putc(ch,f2);
-		}
 
+//will pass in a filename complete with full path...needs to
+//take that in and make a full copy of the file to the restore directory listed in headers.h
+void make_clean_copy(char type, char* file_name){
+
+	FILE *str, *cptr;
+	if((str = fopen(file_name,"rb"))==NULL){
+		fprintf(stderr, "cannot read file \n");
 	}
-	fclose(f);
-	fclose(f2);
+	
+	char* output_file = malloc((strlen(RESTOREDIR)+strlen(file_name))*sizeof(char));
+	
+	 sprintf(output_file,"%s%s",RESTOREDIR,file_name);
+	 printf(" output file name is: %s \n",output_file);
+	 cptr = fopen(output_file,"rb");
+	/*if((cptr = fopen(output_file,"wb"))==NULL){
+		fprintf(stderr,"cnnot open output file\n");
+	}*/
+	printf(" output file name is: %s \n",output_file);
+	char b[2];
+	int i;
+	long size = ftell(str);
+	
+	for(i = 0;i<size;i++){
+		fread(b,1,1,str);
+		fwrite(b,1,1,cptr);
+	}
+	
+	fclose(str);
+	fclose(cptr); 
+
 }
 
-void handle_change(char type){
+
+//pass a filename into this function (FILE*)
+//
+void handle_change(char type, FILE* file_name){
 		//create
 		if(type == 'C'){
 			
 			//write fname to LOG_NAME (using LOG_FORMAT)
-			write_to_log('C');
+			write_to_log('C',file_name);
 		}
 		
 		//modify			
@@ -62,14 +78,22 @@ void handle_change(char type){
 			
 			//this option does the original write in write_to_log then closes the file
 			//after that, this method opens both the original log and the copy file and copys the original to the copy as a backup
-			write_to_log('M');
-			make_log_copy('M');
+			write_to_log('M',file_name);
+			//make_clean_copy('M',file_name);
 		}
 		
 		//delete
 		else if (type == 'D'){
 			//save a copy of fname to RESTOREDIR
-			make_log_copy('D');
+			//make_clean_copy('D',file_name);
 			
 		}
-	}
+}
+
+
+int main(){
+	//handle_change('M',file_name);
+	
+	make_clean_copy('M',"logfile.txt");
+	return 0;
+}
