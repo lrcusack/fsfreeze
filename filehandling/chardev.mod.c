1d067b77fd3fda2e26219f4934d0eb02655fdf98
Liam Cusack <lrcusack7@gmail.com>
2013-12-09 01:45:54 -0500

    parse path up to ~

--- filehandling/chardev.mod.c
+++ filehandling/chardev.mod.c
@@ -16,14 +16,14 @@ __attribute__((section(".gnu.linkonce.this_module"))) = {
 static const struct modversion_info ____versions[]
 __attribute_used__
 __attribute__((section("__versions"))) = {
-	{ 0x89e24b9c, "struct_module" },
-	{ 0x12da5bb2, "__kmalloc" },
+	{ 0xb499ef18, "struct_module" },
 	{ 0x1d26aa98, "sprintf" },
 	{ 0x19070091, "kmem_cache_alloc" },
 	{ 0xab978df6, "malloc_sizes" },
-	{ 0x40e4fec5, "wake_up_process" },
+	{ 0xdcef6636, "wake_up_process" },
 	{ 0x37a0cba, "kfree" },
-	{ 0x4086729e, "register_chrdev" },
+	{ 0x5640e49e, "freezer" },
+	{ 0x1e8e5fac, "register_chrdev" },
 	{ 0x1b7d4074, "printk" },
 	{ 0xc192d491, "unregister_chrdev" },
 };
@@ -34,4 +34,4 @@ __attribute__((section(".modinfo"))) =
 "depends=";
 
 
-MODULE_INFO(srcversion, "C4143C538F162A991333D2A");
+MODULE_INFO(srcversion, "74DFD312C943BAB98BB9338");
